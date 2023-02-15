e36d9290770b1f760da141e6b017fe439cfffc49
paulabueno <paulabueno.ferreira1@gmail.com>
2023-02-15 16:25:18 +0100

    blosg

--- JS/script.js
+++ JS/script.js
@@ -3,18 +3,18 @@ const posts = [
     {
         title: "Google Chrome",
         content: "O Google Chrome é um navegador de internet desenvolvido pela Google. Foi lançado pela primeira vez em setembro de 2008, para o Microsoft Windows, e mais tarde foi portado para Linux, Mac, iOS e Android.",
-        img: "/Users/paula/Documents/repos/aula4/img/chrome.png"
+        img: "img/chrome.png"
     },
 
     {
         title: "Mozilla Firefox",
-        content: "Mozilla Firefox é um navegador livre e multiplataforma desenvolvido pela Mozilla Foundation com ajuda de centenas de colaboradores."
-    },
+        content: "Mozilla Firefox é um navegador livre e multiplataforma desenvolvido pela Mozilla Foundation com ajuda de centenas de colaboradores.",
+        img: "img/chrome.png"},
 
     {
         title: "Microsft Edge",
-        content: "O Microsoft Edge é um navegador da internet desenvolvido pela Microsoft. Foi lançado pela primeira vez para Windows 10 e Xbox One em 2015, depois para Android e iOS em 2017, para macOS, e distribuições Linux, como Ubuntu, Fedora e OpenSUSE e em suas derivações em 2020."
-    },
+        content: "O Microsoft Edge é um navegador da internet desenvolvido pela Microsoft. Foi lançado pela primeira vez para Windows 10 e Xbox One em 2015, depois para Android e iOS em 2017, para macOS, e distribuições Linux, como Ubuntu, Fedora e OpenSUSE e em suas derivações em 2020.",
+        img: "img/chrome.png"},
 
 ];
 
@@ -31,15 +31,13 @@ for (var i=0; i<posts.length; i++) {
     const titleText = document.createTextNode(postData.title);
     title.appendChild(titleText);
     article.appendChild(title);
-    
+
     const content = document.createElement('p');
     const contentText = document.createTextNode(postData.content);
     content.appendChild(contentText);
     article.appendChild(content);
 
-    const img = document.createElement('img');
-    img.src = postData.img;
-    article.appendChild(img);
+   
     
     wrapper.appendChild(article);
 }
